1583cc4784b1cecb723e13caca31f885ffdde054
Matheus Santos <matheus.cesario.santos@gmail.com>
2015-04-07 00:06:43 -0300

    master: Updating Laravel project to version 4.2

--- app/routes.php
+++ app/routes.php
@@ -7,19 +7,16 @@
 |
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| and give it the Closure to execute when that URI is requested.
 |
 */
 
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::get('users', function() {
-    return 'Users!';
+Route::get('/', function()
+{
+	return View::make('hello');
 });
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::get('users', function()
+{
+    return 'Users!';
+});
